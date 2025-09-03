70ecbdc3502b67991ad97116519ef7e951c0804c
Alessandra-celma <alessandracelma_@hotmail.com>
2025-09-03 17:54:44 -0400

    commit

--- index.js
+++ index.js
@@ -5,25 +5,16 @@ class Heroi {
         this.tipo = tipo;
     }
     atacar() {
-        let ataque;
-
-        switch (this.tipo) {
-            case "mago":
-                ataque = "usou magia";
-                break;
-            case "guerreiro":
-                ataque = "usou espada";
-                break;
-            case "monge":
-                ataque = "usou artes marciais";
-                break;
-            case "ninja":
-                ataque = "usou shuriken";
-                break;
-            default:
-                ataque = "usou um ataque desconhecido";
-        }
-        console.log('"${this.tipo} atacou usando ${ataque}"');
+         const ataques = {
+            "o mago": "explosão arcana",
+            "a guerreira": "lâmina",
+            "o monge": "soco de ferro",
+            "o ninja": "katana"
+        };
+        const ataque = ataques[this.tipo.toLowerCase()];
+        if (ataque) {
+            console.log(`${this.tipo}, atacou usando ${ataque}`);
+         }
     }
 }
 // Exemplos de uso
@@ -33,7 +24,7 @@ const heroi3 = new Heroi("ju cook", 80, "O monge");
 const heroi4 = new Heroi("Zenkai", 29, "O ninja");
 
 
-heroi1.atacar(); // Output: guerreiro, atacou usando a lãmina flamejante
+heroi1.atacar(); // Output: guerreiro, atacou usando a lãmina 
 heroi2.atacar(); // Output: mago, atacou usando explosão arcana
 heroi3.atacar(); // Output: monge, atacou usando soco de ferro
 heroi4.atacar(); // Output: ninja, atacou usando katana
